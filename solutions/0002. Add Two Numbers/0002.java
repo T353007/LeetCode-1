a4936418ea94b8c4d384a91aa43f034c87f3fae5
T353007 <alamimtiaz@hotmail.com>
2021-01-12 00:38:18 -0800

    Update 0002.java

--- solutions/0002. Add Two Numbers/0002.java
+++ solutions/0002. Add Two Numbers/0002.java
@@ -1,3 +1,26 @@
+/*
+https://leetcode.com/problems/add-two-numbers/
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored
+in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the
+sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+*/
+
 class Solution {
   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
     ListNode dummy = new ListNode(0);
@@ -21,3 +44,14 @@ class Solution {
     return dummy.next;
   }
 }
+
+/*
+
+Complexity Analysis
+
+Time complexity : O(\max(m, n))O(max(m,n)). Assume that mm and nn represents the length of l1l1 and l2l2
+respectively, the algorithm above iterates at most \max(m, n)max(m,n) times.
+
+Space complexity : O(\max(m, n))O(max(m,n)). The length of the new list is at most \max(m,n) + 1max(m,n)+1.
+
+*/
